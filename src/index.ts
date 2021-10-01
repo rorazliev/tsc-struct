69db23a2cfcf0d3cd677a4cb7473da1b7cd791ec
Ruslan Orazliev <rorazliev@outlook.com>
2021-09-30 21:59:29 -0500

    Export Hash Table

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import HashTable from './hash-table/HashTable';
 import LinkedList from './linked-list/LinkedList';
 import LinkedListNode from './linked-list/LinkedListNode';
 import Queue from './queue/Queue';
 import Stack from './stack/Stack';
 
 export {
-  LinkedList, LinkedListNode, Queue, Stack,
+  HashTable, LinkedList, LinkedListNode, Queue, Stack,
 };
