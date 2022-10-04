6aa94d823f46bbcf1cab0f38c9234ebb63d8d40f
Ruslan Orazliev <rorazliev@outlook.com>
2022-10-03 22:34:22 -0400

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+	bail: false,
+	collectCoverage: true,
+	coverageDirectory: './coverage/',
+	coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
+	testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+	testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.ts$',
+	verbose: true,
+};
+
+export default config;
